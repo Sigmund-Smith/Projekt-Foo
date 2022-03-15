053a12db1653504d075e6189ddafad04ca43f419
Sigmund-Smith <101128192+Sigmund-Smith@users.noreply.github.com>
2022-03-15 15:23:50 +0100

    added comment

--- readcsvanswers.py
+++ readcsvanswers.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 import pandas as pd
-from secrets import *
+from secrets import *  # format von secrets.py: csv_link = r"[google.docs.link]"
 
 dataframe = pd.read_csv(csv_link)
 print(dataframe)
