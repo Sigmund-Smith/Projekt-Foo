b5532de9a044dba0a37253a0bd1a3e5452ca4fc9
Sigmund-Smith <github@leanderraoul.de>
2022-03-17 13:14:23 +0100

    added comments

--- googlesheets_to_dataframe.py
+++ googlesheets_to_dataframe.py
@@ -2,18 +2,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import gspread
 
-
+# sagt der google api wo sie suchen soll
 scope = [
 'https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive'
 ]
 
-GOOGLE_SHEETS_KEY_FILE = 'the-fooprojekt-1c98624c8370.json'
+#dies hier bestätigt google, dass das program auch wirklich auf die tabelle zugreifen darf
+GOOGLE_SHEETS_KEY_FILE = 'the-fooprojekt-1c98624c8370.json' # pls ask admin for file
 credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_SHEETS_KEY_FILE, scope)
 gc = gspread.authorize(credentials)
 
-
-sheet = gc.open("Vorläufiges Endergebnis (Antworten)").sheet1
-dataframe = sheet.get_all_records()
-dataframe = pd.DataFrame(dataframe)
-print(dataframe)
+# wählt die richtige Tabelle aus
+sheet = gc.open("Umfragedaten").sheet1
+dataframe_data = pd.DataFrame(sheet.get_all_records())
+#dataframe = pd.DataFrame(dataframe)
+print(dataframe_data) # high iq debugging
