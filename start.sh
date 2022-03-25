d91c93f0645412fe0b0ce89ff77480c691fe5f83
Sigmund-Smith <101128192+Sigmund-Smith@users.noreply.github.com>
2022-03-25 11:06:45 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#funktioniert erst nach der erfolgreichen Ausführung von setup.sh
+
+git pull
+
+source python_umgebung/bin/activate
+python test.py # oder welches file auch immer am Anfang ausgeführt wird
+
+deactivate
+echo "Danke, dass sie sich die Zeit genommen haben"
